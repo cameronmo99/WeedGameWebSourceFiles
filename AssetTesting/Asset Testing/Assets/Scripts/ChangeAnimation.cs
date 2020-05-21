18a33298c800636209c93196b84c4915b96fd651
Cameronmo99 <cameronmo99@gmail.com>
2020-05-21 22:19:02 +0200

    Asset Test

---
+++ AssetTesting/Asset Testing/Assets/Scripts/ChangeAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimation : MonoBehaviour
+{
+    public Animation Run;
+    public Animation Jump;
+    public GameObject Player;
+
+    public void Start()
+    {
+        Player.GetComponent<Animation>().Play("Root│Jump");
+    }
+
+    public void RunAnimation()
+    {
+        Player.GetComponent<Animation>().Play("Root│Run");
+
+    }
+}
---
+++ AssetTesting/Asset Testing/Assets/Scripts/Changemodel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changemodel : MonoBehaviour
+{
+    public Texture Criminal;
+    public Texture Cyborg;
+    public Texture SkaterM;
+    public Texture SkaterF;
+
+
+    public GameObject Player;
+    public Renderer PlayerRenderer;
+
+
+    public void ChangeModelCriminal()
+    {
+        PlayerRenderer.material.SetTexture("_MainTex", Criminal);
+    }
+
+    public void ChangeModelCyborg()
+    {
+        PlayerRenderer.material.SetTexture("_MainTex", Cyborg);
+    }
+
+    public void ChangeModelSkaterM()
+    {
+        PlayerRenderer.material.SetTexture("_MainTex", SkaterM);
+    }
+
+    public void ChangeModelSkaterF()
+    {
+        PlayerRenderer.material.SetTexture("_MainTex", SkaterF);
+    }
+}
